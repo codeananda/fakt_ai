31e7fbfbd56654e555b3469fe26968c1ea931c88
codeananda <51246969+codeananda@users.noreply.github.com>
2024-09-09 18:24:06 +0200

    Switched to use create_react_agent but it's too brittle

--- fakt_ai/app.py
+++ fakt_ai/app.py
@@ -3,14 +3,14 @@ import os
 import gradio as gr
 from dotenv import load_dotenv
 from gradio import ChatMessage
-from langchain import hub
-from langchain.agents import AgentExecutor, create_openai_tools_agent
-from langchain_community.agent_toolkits.load_tools import load_tools
-from langchain_openai import ChatOpenAI
-from fakt_ai.crewai_implementation import get_all_tools
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.prompts import PromptTemplate
+
+from fakt_ai.crewai_implementation import get_all_tools, _get_llm
 
 load_dotenv()
 
+# TODO - add elegant section for all API keys
 if not (os.getenv("SERPAPI_API_KEY") and os.getenv("OPENAI_API_KEY")):
     with gr.Blocks() as demo:
         gr.Markdown(
@@ -23,21 +23,75 @@ if not (os.getenv("SERPAPI_API_KEY") and os.getenv("OPENAI_API_KEY")):
         )
     demo.launch()
 
-model = ChatOpenAI(temperature=0, streaming=True)
+model = _get_llm("groq")
 
 tools = get_all_tools()
 
-# Get the prompt to use - you can modify this!
-prompt = hub.pull("hwchase17/openai-tools-agent")
-# print(prompt.messages) -- to see the prompt
-agent = create_openai_tools_agent(model.with_config({"tags": ["agent_llm"]}), tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools).with_config({"run_name": "Agent"})
+prompt = PromptTemplate.from_template(
+    """
+    Answer the following questions as best you can. You have access to the following tools:
+    {tools} 
+    
+    with the following names:
+    {tool_names}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    You are Fakt AI, an agent designed to scour the internet and determine whether a given
+    user query is True or False. You exist to cut through the noise and combat the mass of
+    information online that no single person can possibly handle themselves. It is your goal
+    to gather relavant info and synthesis it into comprehensive answers.
+
+    Some use cases include:
+    - Alice listens to a podcast and hears Bob say: X is true and you can read scientific
+    paper Y for proof. 
+
+        - Fakt AI, will search online for that paper, read the paper, find the relevant portions
+        and present back an argument as to whether that is true or not.
+
+    It is perfectly fine to return back that a statement is False. 
+
+    We rely on cold, hard facts and do not shy away from the tense topics of the day e.g.
+    migration, vaccines, trans activism etc. 
+
+    Our goal is to present the Truth as the current scientific literature understands it.
+    We fully understand that this is always a 'best guess'.
+
+    We do not purport to be able to answer all questions. Our goal is simply to gather
+    the relevant data and present it in a way that is easy for a lay person to understand.
+
+    In this, we fulfil a crucial role that most people today lack: the aility to perform
+    deep research across the web, read technical papers from disparate disciplines, and 
+    present conclusions. 
+
+    Here is the query we will be working with:
+    {query}
+
+    Thought:{agent_scratchpad}
+    """
+)
+
+agent = create_react_agent(model, tools, prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    handle_parsing_errors=True,
+)
 
 
 async def interact_with_langchain_agent(prompt, messages):
     messages.append(ChatMessage(role="user", content=prompt))
     yield messages
-    async for chunk in agent_executor.astream({"input": prompt}):
+    async for chunk in agent_executor.astream({"query": prompt}):
         if "steps" in chunk:
             for step in chunk["steps"]:
                 messages.append(
